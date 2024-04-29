5d9b1eb1d68882105c39c026230c409d570041a6
Shootie <jose.f.rocha92@gmail.com>
2024-04-29 15:37:19 +0100

    Add the new lvl design for future updates

---
+++ .resources/lvl-design/lvl3/decorsx4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="decorsx4" tilewidth="64" tileheight="64" tilecount="98" columns="14">
+ <image source="Decorsx4.png" width="896" height="448"/>
+</tileset>
---
+++ .resources/lvl-design/lvl_map/tile1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map tile" tilewidth="64" tileheight="64" tilecount="48" columns="8">
+ <image source="../Untitled-1.png" width="512" height="384"/>
+</tileset>
